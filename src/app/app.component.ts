83fb24b430be050aa6017a9f5aaee6e15d89de23
Said Berk <csaidberk@gmail.com>
2024-07-18 17:36:58 +0300

    Amplify UI removed from app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { LandingScreenComponent } from "./landing-screen/landing-screen.component";
-import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
 
 Amplify.configure(outputs);
 
@@ -12,7 +12,7 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, RouterLink, LandingScreenComponent, AmplifyAuthenticatorModule],
+  imports: [RouterOutlet, RouterLink, LandingScreenComponent],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
