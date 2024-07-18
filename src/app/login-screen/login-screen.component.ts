323c701d360c4fb310153f0c41128ce723b2607e
Said Berk <csaidberk@gmail.com>
2024-07-18 16:01:08 +0300

    Login Screen UI designed.

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
 @Component({
   selector: 'app-login-screen',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    CheckboxModule
+  ],
   templateUrl: './login-screen.component.html',
   styleUrl: './login-screen.component.css'
 })
 export class LoginScreenComponent {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    rememberMe: [false]
+  });
 
+  constructor(private fb: FormBuilder) { }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Burada giriş işlemini gerçekleştirin
+      console.log(this.loginForm.value);
+    }
+  }
 }
